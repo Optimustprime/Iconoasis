dec3892a3bcd09a32794f5c7d78e9b8ca8a72a87
ajadi <ajadimarvellousgo@gmail.com>
2023-06-02 04:04:39 +0100

    .

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,13 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1x)!39m!4mx=mka!j@v7nwokms-ui!e!nswc+0xpjix22f8k$6'
+SECRET_KEY = os.environ.get('SECRET_KEY','changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',0)))
 
 ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS','').split(',')
+    )
+)
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework.authtoken',
-    'recipe',
     'rest_framework',
     'drf_spectacular',
     'user',
