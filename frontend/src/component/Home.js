d7e5faa8ca3a1ea2303d1d05252b830ae8a85d07
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-06-09 07:31:28 +0100

    frontend change

--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -1,14 +1,11 @@
 import NavBar from '../component/NavBar/Navbar';
 import SearchFilter from "../component/SearchFilter/SearchFilter";
-import NounProjectSearch from "./SearchFilter/test";
-import MyComponent from "./SearchFilter/test";
 
 export default  function Home() {
     return (
         <div className='force'>
             <NavBar/>
             <SearchFilter/>
-            <MyComponent/>
         </div>
     );
 }
