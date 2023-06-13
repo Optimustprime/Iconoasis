b37307e717456b4e39a5b989db8a06718892f588
ajadi <ajadimarvellousgo@gmail.com>
2023-06-13 06:26:37 +0100

    .

--- frontend/src/component/SignUp/axiosWithAuth.jsx
+++ frontend/src/component/SignUp/axiosWithAuth.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const axiosWithAuth = axios.create({
-    baseURL: 'http://ec2-3-21-134-116.us-east-2.compute.amazonaws.com/api/',
+    baseURL: 'http://ec2-3-131-97-133.us-east-2.compute.amazonaws.com/api/',
     headers: {
         Authorization: `Token ${localStorage.getItem('ent')}`
     },
